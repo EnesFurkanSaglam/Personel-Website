da9301dab8516d449bc5df703f42486bd4bc388f
Enes Furkan SAGLAM <enesfurkansaglam@gmail.com>
2024-06-12 16:30:06 +0300

    Finished MySkills

--- react-js-personelwebsite/src/Pages/Home/Homescreen/index.jsx
+++ react-js-personelwebsite/src/Pages/Home/Homescreen/index.jsx
@@ -1,9 +1,21 @@
-import HeroSections from "../HeroSection";
+import AboutMe from "../AboutMe";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
+import HeroSection from "../HeroSection";
+import MyPortfolio from "../MyPortfolio";
+import MySkills from "../MySkills";
+import Testimonial from "../Testimonials";
 
-export default function Home(){
-    return(
-        <>
-        <HeroSections/>
-        </>
-    );
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <MySkills />
+      <AboutMe />
+      <MyPortfolio />
+      <Testimonial />
+      <ContactMe />
+      <Footer />
+    </>
+  );
 }
